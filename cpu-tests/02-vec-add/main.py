888549794ff4c78bccbd5fcf70245df171ada92e
Tong <tz@gatech.edu>
2024-07-04 03:46:39 -0400

    added new files

--- cpu-tests/02-vec-add/main.py
+++ cpu-tests/02-vec-add/main.py
@@ -3,21 +3,24 @@ import importlib
 import numpy as np
 from timeit import timeit
 
-# Compile and load the cpp module
+# 编译pybind11模块并动态导入
 name = 'vec_add'
-kernel = 'kernels/vec_add.cpp'
-cmd = f'g++ -O3 -Wall -shared -std=c++11 -fPIC -fopenmp $(python3 -m pybind11 --includes) {kernel} -o {name}$(python3-config --extension-suffix)'
+cmd = f'g++ -O3 -Wall -shared -std=c++11 -fPIC -fopenmp $(python3 -m pybind11 --includes) {name}.cpp -o {name}$(python3-config --extension-suffix)'
 ret = os.system(cmd)
 if ret != 0:
-    print('compile cpp file failed')
-
+    print('compiling cpp file failed')
 my_package = importlib.import_module(name)
+
+# 生成输入数组a和b
 N = 200000
 a = np.random.randn(N)
 b = np.random.randn(N)
+
+# 调用add函数并使用allclose确认结果正确
 c = my_package.add(a, b)
 assert np.allclose(c, a+b)
 
+# 性能测试
 t0 = timeit("my_package.add(a, b)", globals=globals(), number=50)
 t1 = timeit("a + b", globals=globals(), number=50)
 print(t0, t1)

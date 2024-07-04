e12ea200e8839f5f783dc7996482d4b80b124ff4
Tong <tz@gatech.edu>
2024-07-04 03:00:32 -0400

    renamed files

--- cpu-tests/kernels/example.cpp
+++ cpu-tests/kernels/example.cpp
@@ -8,6 +8,5 @@ int add(int i, int j) {
 
 PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
-
     m.def("add", &add, "A function that adds two numbers");
 }

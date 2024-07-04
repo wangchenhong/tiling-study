6568fa3d2a11261e291fa9e0300fff396506f10d
Tong <tz@gatech.edu>
2024-07-04 03:03:34 -0400

    reorganized

--- cpu-tests/01-add-two-numbers/main.py
+++ cpu-tests/01-add-two-numbers/main.py
@@ -1,8 +1,8 @@
 import os
 import importlib
 
-# 假定刚才的C++文件叫example.cpp，处在kernels文件夹下
-kernel = 'kernels/example.cpp'  
+# 假定刚才的C++文件叫example.cpp
+kernel = 'example.cpp'  
 # 编译命令
 cmd = f'g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) {kernel} -o example$(python3-config --extension-suffix)'
 # 执行编译命令

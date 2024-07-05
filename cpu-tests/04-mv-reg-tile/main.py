dab9473521d784928b5b57450cb4c84ab62cffbc
Tong <tz@gatech.edu>
2024-07-05 11:34:21 -0400

    I realized that with N=16000, the vector is going to fit in L2 cache, and the data acess bottle neck is in loading matrix A. Thus, trying to do tiling is not going to help much because it tries to reduces accesses to L2 cache, but loading matrix A is taking most of the time

--- cpu-tests/04-mv-reg-tile/main.py
+++ cpu-tests/04-mv-reg-tile/main.py
@@ -12,15 +12,15 @@ if ret != 0:
 my_package = importlib.import_module(name)
 
 # 生成输入数组a和b
-N = 16000
-a = np.random.randn(N, N)
+N = 16000 * 4
+a = np.random.randn(N // 8, N)
 b = np.random.randn(N)
 
 # 调用add函数并使用allclose确认结果正确
-c = my_package.kernel(a, b)
+c = my_package.kernel0(a, b)
 assert np.allclose(c, a@b)
 
 # 性能测试
-t0 = timeit("my_package.kernel(a, b)", globals=globals(), number=100)
-t1 = timeit("a @ b", globals=globals(), number=100)
+t0 = timeit("my_package.kernel0(a, b)", globals=globals(), number=10)
+t1 = timeit("a @ b", globals=globals(), number=10)
 print(t0, t1)
--- cpu-tests/04-mv-reg-tile/mv.cpp
+++ cpu-tests/04-mv-reg-tile/mv.cpp
@@ -4,6 +4,26 @@
 
 namespace py = pybind11;
 
+py::array_t<double> kernel0(py::array_t<double> a, py::array_t<double> b) {
+    auto M = a.shape(0);
+    auto N = a.shape(1);
+    auto c = py::array_t<double>(M);
+    auto a_ptr = a.mutable_data();
+    auto b_ptr = b.mutable_data();
+    auto c_ptr = c.mutable_data();
+
+    #pragma omp parallel for num_threads(16)
+    for (int i = 0; i < M; i++) {
+        double sum = 0;
+        for (int j = 0; j < N; j++) {
+            sum += a_ptr[i * N + j] * b_ptr[j];
+        }
+        c_ptr[i] = sum;
+    }
+
+    return c;
+}
+
 py::array_t<double> kernel(py::array_t<double> a, py::array_t<double> b) {
     auto M = a.shape(0);
     auto N = a.shape(1);
@@ -38,5 +58,6 @@ py::array_t<double> kernel(py::array_t<double> a, py::array_t<double> b) {
 
 PYBIND11_MODULE(mv, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
-    m.def("kernel", &kernel, "A function that adds two numbers");
+    m.def("kernel", &kernel, "");
+    m.def("kernel0", &kernel0, "");
 }

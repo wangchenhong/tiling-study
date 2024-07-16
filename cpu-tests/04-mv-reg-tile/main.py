a7d628eb85615f1e928d4cc22536dd6cd2461842
Tong <tz@gatech.edu>
2024-07-16 04:08:15 -0400

    accordig to my tests, my own benchmarking tool is the most stable one

--- cpu-tests/04-mv-reg-tile/main.py
+++ cpu-tests/04-mv-reg-tile/main.py
@@ -1,7 +1,9 @@
 import os
 import importlib
 import numpy as np
-from timeit import timeit
+from timeit import timeit, repeat
+import time
+import numba
 
 # 编译pybind11模块并动态导入
 name = 'mv'
@@ -11,20 +13,70 @@ if ret != 0:
     print('compiling cpp file failed')
 my_package = importlib.import_module(name)
 
+import torch.utils.benchmark as torchbench
 
-for N in [1024*1024, 1024*1024*2, 1024*1024*4]:
-    M = 1024
+def bench(fn):
+    t0 = torchbench.Timer(
+        stmt='fn()',
+        globals={'fn': fn},
+    )
+    return t0.blocked_autorange(min_run_time=1).mean
+
+def mybench(fn, clear_cache=True, n_warmup=10, n_iters=30):
+    times = []
+    for i in range(n_iters+n_warmup):
+        if clear_cache:
+            _ = np.zeros(256*1024*1024//8, dtype=np.float64)
+        t0 = time.perf_counter()
+        fn()
+        elapsed = time.perf_counter() - t0
+        times.append(elapsed)
+    return np.mean(times[n_warmup:])
+
+@numba.njit(parallel=True)
+def gen_matrix(M, N, dtype=np.float64):
+    out = np.empty((M, N), dtype=dtype)
+    for i in numba.prange(M):
+        out[i] = np.random.randn(N)
+    return out
+
+for N in [1024*1024*8, 1024*1024*4, 1024*1024*2, 1024*1024*1, 1024*1024//2]:
+    M = 512
     # 生成输入数组a和b
-    a = np.random.randn(M, N)
+    a = gen_matrix(M, N)
+    #a = np.random.randn(M, N)
     b = np.random.randn(N)
+    print('done generating inputs')
 
     # 调用add函数并使用allclose确认结果正确
     c = my_package.kernel(a, b)
     assert np.allclose(c, a@b)
 
     # 性能测试
-    t0 = timeit("my_package.kernel0(a, b)", globals=globals(), number=10)
-    t1 = timeit("my_package.kernel(a, b)", globals=globals(), number=10)
-    t2 = timeit("a @ b", globals=globals(), number=10)
+    niters = 10
+    t0 = repeat("my_package.kernel0(a, b)", globals=globals(), number=niters, repeat=10)
+    t1 = repeat("my_package.kernel(a, b)", globals=globals(), number=niters, repeat=10)
+    t2 = repeat("a @ b", globals=globals(), number=niters, repeat=10)
+    print(t0[5:])
+    print(t1[5:])
+    print(t2[5:])
+    #print(np.mean(t0) / niters, np.mean(t1) / niters, np.mean(t2) / niters)
+    #print(np.std(t0), np.std(t1), np.std(t2))
+    
+    #print(t0, t1, t2)
+    #print(f'speedup with tiling: {(t0/t1):.4f}')
+
+    t0 = bench(lambda: my_package.kernel0(a, b))
+    t1 = bench(lambda: my_package.kernel(a, b))
+    t2 = bench(lambda: a @ b)
     print(t0, t1, t2)
-    print(f'speedup with tiling: {(t0/t1):.4f}')
+
+    t0 = mybench(lambda: my_package.kernel0(a, b))
+    t1 = mybench(lambda: my_package.kernel(a, b))
+    t2 = mybench(lambda: a @ b)
+    print(t0, t1, t2)
+    print(f'speedup with tiling: {(t0/t1):.4f}\n')
+    print()
+    # print(f'speedup with tiling: {(t0[0]/t1[0]):.4f}\n')
+
+    
--- cpu-tests/04-mv-reg-tile/mv.cpp
+++ cpu-tests/04-mv-reg-tile/mv.cpp
@@ -4,6 +4,9 @@
 
 namespace py = pybind11;
 
+#define BM 32
+#define BN 32
+
 py::array_t<double> kernel0(py::array_t<double> a, py::array_t<double> b) {
     auto M = a.shape(0);
     auto N = a.shape(1);
@@ -51,6 +54,42 @@ py::array_t<double> kernel(py::array_t<double> a, py::array_t<double> b) {
         c_ptr[i+1] = c1;
         c_ptr[i+2] = c2;
         c_ptr[i+3] = c3;
+    }
+
+    return c;
+}
+
+py::array_t<double> kernel_l1(py::array_t<double> a, py::array_t<double> b) {
+    auto M = a.shape(0);
+    auto N = a.shape(1);
+    auto c = py::array_t<double>(M);
+    auto a_ptr = a.data();
+    auto b_ptr = b.data();
+    auto c_ptr = c.mutable_data();
+
+    #pragma omp parallel for num_threads(64)
+    for (int ii = 0; ii < M; ii += BM) {
+        for (int jj = 0; jj < N; jj += BN) {
+            for (int i = ii; i < ii+BM; i+=4) {
+                double c0 = 0;
+                double c1 = 0;
+                double c2 = 0;
+                double c3 = 0;
+                
+                for (int j = jj; j < jj+BN; j++) {
+                    double b0 = b_ptr[j];
+
+                    c0 += a_ptr[i * N + j] * b0;
+                    c1 += a_ptr[(i+1) * N + j] * b0;
+                    c2 += a_ptr[(i+2) * N + j] * b0;
+                    c3 += a_ptr[(i+3) * N + j] * b0;
+                }
+                c_ptr[i] = c0;
+                c_ptr[i+1] = c1;
+                c_ptr[i+2] = c2;
+                c_ptr[i+3] = c3;
+            }
+        }
     }
 
     return c;
